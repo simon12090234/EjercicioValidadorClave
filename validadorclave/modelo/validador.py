447ddc522001de30b122796b3462d13a35cabfd3
simon12090234 <corrasimon82@gmail.com>
2024-10-16 13:12:16 -0500

    creacion metodos _validar_longitud _contiene_mayuscula _contiene_minuscula _contiene_numero

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,21 +1,40 @@
 from abc import ABC, abstractmethod
 from errores import *
 
+
 class ReglaValidacion(ABC):
     def init(self, longitud_esperada: int):
         self._longitud_esperada: int = longitud_esperada
 
-
     @abstractmethod
     def es_valida(self):
         pass
+
     def _validar_longitud(self, clave):
-        try:
-            if len(clave) > self._longitud_esperada:
+        count_1 = 0
+        for i in clave:
+            count_1 += 1
+
+        if count_1 > self._longitud_esperada:
+            return True
+
+    def _contiene_mayuscula(self, clave):
+        for item in clave:
+            if item.isupper():
                 return True
+        return False
 
-            else:
-                raise NoCumpleLongitudMinimaError
+    def _contiene_minuscula(self, clave):
+        for item in clave:
+            if item.islower():
+                return True
+        return False
 
-        except():
-            pass
+    def _contiene_numero(self, clave):
+        for item in clave:
+            if item.isdigit():
+                return True
+        return False
+
+
+

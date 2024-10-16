a4c9fd7ad59c8ef91094e46c9157ee56fce7a1d4
simon12090234 <corrasimon82@gmail.com>
2024-10-16 12:54:31 -0500

    Se ha creado la clase ReglaValidacion con su constructor y su abstract method

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,21 @@
-# TODO: Implementa el código del ejercicio aquí
+from abc import ABC, abstractmethod
+from errores import *
+
+class ReglaValidacion(ABC):
+    def init(self, longitud_esperada: int):
+        self._longitud_esperada: int = longitud_esperada
+
+
+    @abstractmethod
+    def es_valida(self):
+        pass
+    def _validar_longitud(self, clave):
+        try:
+            if len(clave) > self._longitud_esperada:
+                return True
+
+            else:
+                raise NoCumpleLongitudMinimaError
+
+        except():
+            pass
